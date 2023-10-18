4117f650cc7006ff07e4f72764faf0c78bb11514
jino.michel.aque <contact@geeklogbook.com>
2023-10-17 23:15:09 -0300

    add App.cs

---
+++ App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace libreria_app
+{
+    class App
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Librería Abierta");
+        }
+    }
+}
